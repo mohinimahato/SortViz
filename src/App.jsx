1140f74029cacfe9e364c0a858ce80a95be77826
Mohini Mahato <mohinimahatowork@gmail.com>
2023-07-29 19:58:57 +0530

    Add: initial files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+     
+      </div>
+      <h1>SortViz</h1>
+    </>
+  )
+}
+
+export default App
